30344fc35aaa00437be4bc63d1e76a9ca96ecb81
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-09-16 22:18:43 -0700

    commit tree eed6e8fb385e48b2c63e49e62118cdce3986d46e

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+class Polygon {
+  constructor(array) {
+    this.sides = array;
+  }
+
+  get countSides() {
+    return this.sides.length;
+  }
+
+  get perimeter() {
+    return this.sides.reduce((prev, val) => prev + val);
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    const valid1 = this.sides[0] + this.sides[1] > this.sides[2] ? true : false;
+    const valid2 = this.sides[1] + this.sides[2] > this.sides[0] ? true : false;
+    if (valid1 && valid2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    return this.sides.every((side) => side === this.sides[0]);
+  }
+
+  get area() {
+    return this.sides[0] ** 2;
+  }
+}
